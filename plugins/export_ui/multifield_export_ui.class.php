1b251cb0f2b66847e083ffe58c501889aba7520a
Dave Reid <dave@davereid.net>
2013-09-07 14:08:29 -0500

    Add note about fields and field data being deleted on multifield delete confirm form.

--- plugins/export_ui/multifield_export_ui.class.php
+++ plugins/export_ui/multifield_export_ui.class.php
@@ -7,4 +7,14 @@ class multifield_export_ui extends ctools_export_ui {
     }
     return parent::access($op, $item);
   }
+
+  function delete_page($js, $input, $item) {
+    $output = parent::delete_page($js, $input, $item);
+
+    if ($fields = multifield_type_get_fields($item)) {
+      $output['description']['#markup'] .= ' ' . t('The following fields and their respective data will also be removed, which cannot be undone:') . theme('item_list', array('items' => $fields));
+    }
+
+    return $output;
+  }
 }
